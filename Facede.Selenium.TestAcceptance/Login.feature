0a817caab0adf3fef7ff0e8ef1ea59f5f8b1ffd5
Luã Govinda <govinda777@gmail.com>
2016-05-10 13:31:54 -0300

    Implementação do Spacflow

---
+++ Facede.Selenium.TestAcceptance/Login.feature
@@ -0,0 +1,11 @@
+﻿Funcionalidade: Login
+				Para um usario e senha verificamos se he efetuado o login
+		
+
+@TestAcceptanceLogin
+Cenário: Login na aplicacao com dados validos
+	Dado Que possuo usuario e senha validos
+	Quando For me logar na aplicacao com os dados
+		| Usuario                     | Senha          |
+		| luan_govinda777@hotmail.com | Hermeserenato1 |
+	Então Conseguirei me logar
---
+++ Facede.Selenium.TestAcceptance/LoginStepDefinition.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechTalk.SpecFlow;
+
+namespace Facede.Selenium.TestAcceptance
+{
+    [Binding]
+    public class LoginStepDefinition
+    {
+        [Given(@"Que possuo usuario e senha validos")]
+        public void DadoQuePossuoUsuarioESenhaValidos()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"For me logar na aplicacao com os dados")]
+        public void QuandoForMeLogarNaAplicacaoComOsDados(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"Conseguirei me logar")]
+        public void EntaoConseguireiMeLogar()
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
+
